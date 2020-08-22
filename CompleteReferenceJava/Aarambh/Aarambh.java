9209ee1ab7c83258556506ad9a69d205cc06aea9
Rahul Agrawal <agr.rahul28@gmail.com>
2020-08-22 20:20:35 +0530

    First Program in Java

---
+++ CompleteReferenceJava/Aarambh/Aarambh.java
@@ -0,0 +1,14 @@
+package Aarambh;
+
+/**
+ * Aarambh.java
+ *
+ * Starting the Jounrey into Java
+ *
+ */
+
+class Aarambh {
+    public static void main(String args[]) {
+	System.out.println("!!! Hare Rama Hare Krishna !!!");
+    }
+}
